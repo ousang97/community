a00c4e6a8f4db6a30868bb9a875dd8a48132a3d5
ousang97 <474572172@qq.com>
2019-10-13 17:27:46 +0800

    init report

---
+++ src/main/java/com/wwtt/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.wwtt.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Cread by wwtt
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
